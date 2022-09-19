e90ac9b53fbdd6c14b1dbcddf5a0d3d12bc92c71
n-yata <yata1214@gmail.com>
2022-09-19 09:40:40 +0900

    batファイルの統合

--- copyJar.bat
+++ copyJar.bat
@@ -1,17 +1,29 @@
 @echo off
 
-set copyJar1=complete-0.1-all.jar
-set copyJar2=complete-0.1-runner.jar
+set commonJar=complete-0.1-all.jar
+set subCommonJar=complete-0.1-runner.jar
 
-set nonExistsDir1=dir4
-set nonExistsDir2=dir5
+set commonDir=dir4
+set subCommonDir=dir5
 
+:: jarの削除
 for /d %%d in (*) do (
 
     echo dirname = %%d
 
-    if not %%d == %nonExistsDir1% if not %%d == %nonExistsDir2% (
-        copy %copyJar1% %%d\opt\lib\%copyJar1%
-        copy %copyJar2% %%d\opt\lib\%copyJar2%
+    del %%d\opt\lib\%copyJar1%
+    del %%d\opt\lib\%copyJar2%
+    
+    echo delete completed
+)
+
+:: jarのコピー
+for /d %%d in (*) do (
+
+    echo dirname = %%d
+
+    if not %%d == %commonDir% if not %%d == %subCommonDir% (
+        copy %commonJar% %%d\opt\lib\%commonJar%
+        copy %subCommonJar% %%d\opt\lib\%subCommonJar%
     )
 )
